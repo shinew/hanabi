7cfc658be31ccd041b047d8bf1355cd895608ab5
Shine Wang <shine.sw.wang@gmail.com>
2017-10-05 21:12:26 -0400

    z

--- server/engine.py
+++ server/engine.py
@@ -1,8 +1,8 @@
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from enum import Enum
-import random
+from random import sample
 
-NUMBER_DISTRIBUTION = {
+VALUE_DISTRIBUTION = {
     1: 3,
     2: 2,
     3: 2,
@@ -10,19 +10,50 @@ NUMBER_DISTRIBUTION = {
     5: 1,
 }
 
+NUM_TIME_TOKENS = 8
+
+NUM_BOMBS = 4
+
 Color = Enum('Color', 'WHITE RED YELLOW GREEN BLUE')
 
 Card = namedtuple('Card', 'color value')
 
-class Deck:
-    def __init__(self, shuffle_seed=random.random):
-        self.cards = []
-        for color in Color:
-            for value in range(1, 6):
-                for _ in range(NUMBER_DISTRIBUTION[value]):
-                    self.cards.append(Card(color, value))
-        random.shuffle(self.cards, shuffle_seed)
+GameState = Enum('GameState', 'NOT_STARTED PLAYING FINISHED')
+
+def gen_cards(shuffle):
+    cards = []
+    for color in Color:
+        for value in range(1, 6):
+            for _ in range(VALUE_DISTRIBUTION[value]):
+                cards.append(Card(color, value))
+    return shuffle(cards)
 
 class Game:
-    def __init__(self, players):
-        pass
+    def __init__(
+            self,
+            gid,
+            pids=[],
+            shuffle=lambda x: sample(x, k=len(x)),
+            time_tokens=NUM_TIME_TOKENS,
+            bombs=NUM_BOMBS):
+        self.gid = gid
+        self.pids = pids
+        self.shuffle = shuffle
+        self.deck = gen_cards(shuffle)
+        self.time_tokens = time_tokens
+        self.bombs = bombs
+        self.playing_index = 0
+        self.hands = defaultdict(list)
+        self.table = defaultdict(int)
+        self.state = GameState.PLAYING
+
+    def add_pid(self, pid):
+        assert self.state == GameState.NOT_STARTED
+        assert 0 <= len(self.pids) <= 4
+        self.pids.append(pid)
+
+    def start(self):
+        assert self.state == GameState.NOT_STARTED
+        assert 2 <= len(self.pids) <= 5
+        self.state = GameState.PLAYING
+        self.pids = self.shuffle(self.pids)
